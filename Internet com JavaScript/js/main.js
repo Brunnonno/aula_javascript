00f25a5ab424c1a615bafd664fbfc7e84feb87a9
Bruno Naiser <brunonaiser@hotmail.com>
2021-10-02 18:06:26 -0300

    Cansei

---
+++ Internet com JavaScript/js/main.js
@@ -0,0 +1,62 @@
+/*
+var lista = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
+console.log(lista[2]);
+*/
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes()); 
+alert(d.getMonth()+1); 
+*/
+/*
+var count;
+for (count=1; count <= 5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count ++;
+}
+*/
+/*
+var idade = prompt("Qual sua idade corno?");
+//var idade = 18;
+if (idade >= 18){
+    alert("ta véio");
+}else{
+    alert("ta criança");
+};
+*/
+/*
+var lista = ["maça","pera","laranja"];
+lista.push("uva geladinha");
+lista.pop();
+console.log(lista);
+console.log(lista.length); Mostra quantidade de elementos que tenho na lista
+console.log(lista.reverse()); Mostra toda a lista de frente para tras 
+console.log(lista.toString()); Mostra no console a lista sem o array
+console.log(lista.join(", ")); Coloca , ou qualquer coisa depois de cada item da lista em string 
+*/
+/*
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas.nome);
+*/
+/*
+LEMBRA SEMPRE DO ;
+var nome = "Bruno Naiser";
+var idade = 19;
+var idade2 = 30;
+var frase = "One Piece é melhor anime do mundo";
+alert(nome +" tem " + idade + " anos " );
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("One Piece", "Naruto"));
+console.log(frase.toUpperCase()) TRANSFORMA TUDO EM MAISCULO
+console.log(frase.toLowerCase()) transforma tudo em minusculo
+alert(frase.replace("One Piece", "Naruto"));
+*/
